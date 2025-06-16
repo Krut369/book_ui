8b5ca97bdaa7cb36faa7f5d7d1bdf48de364b323
Krut369 <krutarthsolanki9@gmail.com>
2025-06-16 23:04:35 +0530

    Updated features in Book UI App

--- lib/main.dart
+++ lib/main.dart
@@ -2,36 +2,32 @@ import 'package:book_grocer/common/color_extenstion.dart';
 import 'package:book_grocer/view/main_tab/main_tab_view.dart';
 import 'package:book_grocer/view/onboarding/onboarding_view.dart';
 import 'package:flutter/material.dart';
+import 'package:device_preview/device_preview.dart'; // ✅ Add this
 
 void main() {
-  runApp(const MyApp());
+  runApp(
+    DevicePreview(
+      builder: (context) => const MyApp(),
+    ),
+  );
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
+      title: 'Book UI App',
       debugShowCheckedModeBanner: false,
+      useInheritedMediaQuery: true, // ✅ Required for DevicePreview
+      locale: DevicePreview.locale(context), // ✅
+      builder: DevicePreview.appBuilder, // ✅
       theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
         primaryColor: TColor.primary,
-      
         fontFamily: 'SF Pro Text',
       ),
-      home: const MainTabView() ,
+      home: const MainTabView(),
     );
   }
 }
-
